463799e60bcb585bf427446e69257376c085bb87
cpoakley1432 <cpoakley1432@gmail.com>
2015-12-02 11:20:25 -0500

    draw ants and make them dance

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -8,12 +8,66 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Main extends Application {
 
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
+    static final int ANT_COUNT = 100;
+
+    ArrayList<Ant> ants;
+    long lastTimeStamp = 0;
+
+    ArrayList<Ant> creanteAnts(){
+        ArrayList<Ant> ants = new ArrayList();
+        for (int i = 0; i <ANT_COUNT; i++) {
+            Random r = new Random();
+            ants.add(new Ant(r.nextInt(WIDTH), r.nextInt(HEIGHT)));
+        }
+        return ants;
+    }
+
+    void drawAnts(GraphicsContext context){
+        context.clearRect(0,0,WIDTH,HEIGHT);
+        for (Ant ant : ants){
+            context.setFill(Color.RED);
+            context.fillOval(ant.x, ant.y,5,5);
+        }
+    }
+
+    double randomStep(){
+        return Math.random()* 2 - 1;
+
+    }
+
+    Ant moveAnt(Ant ant){
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ant.x += randomStep();
+        ant.y += randomStep();
+        return ant;
+    }
+
+    void updateAnts(){
+        ants = ants.parallelStream()
+                .map(this::moveAnt)
+                .collect(Collectors.toCollection(ArrayList<Ant>::new));
+    }
+    int fps(long now){
+        double diff = now - lastTimeStamp;
+        double diffSeconds = diff/1000000000;
+        return (int)(1 / diffSeconds);
+    }
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -26,10 +80,15 @@ public class Main extends Application {
         primaryStage.setTitle("Ants");
         primaryStage.setScene(scene);
         primaryStage.show();
+        ants = creanteAnts();
 
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
+                fpsLabel.setText(String.valueOf(fps(now)));
+                lastTimeStamp = now;
+                updateAnts();
+                drawAnts(context);
 
             }
         };
